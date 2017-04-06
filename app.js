be8010587f965e98b20226acb580054e7b5b110d
Quinn Pan <pjkui@qq.com>
2017-04-06 14:16:25 +0800

    support server

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ App({
     } else {
       //调用登录接口
       wx.login({
-        success: function () {
+        success: function (e) {
+          console.log('wxlogin success!');
+          console.log(e);
+          var code = e.code;
+
           wx.getUserInfo({
             success: function (res) {
               console.log('get User Info');
@@ -21,13 +25,52 @@ App({
               that.globalData.userInfo = res.userInfo;
               that.globalData.userInfo.longitude = 0;
               that.globalData.userInfo.latitude = 0;
+
+
+              //https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+              wx.request({
+                url: 'https://blog.pjkui.com/weapp/index.php',
+                data: {
+                  code: code,
+                  encryptedData: res.encryptedData,
+                  iv: res.iv
+                },
+                method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                }, // 设置请求的 header
+                success: function (res) {
+                  // success
+                  console.log('get user info from server success!');
+                  console.log(res);
+                },
+                fail: function (res) {
+                  console.log('get user info from server failed!');
+                  console.log(res);
+                  // fail
+                },
+                complete: function (res) {
+                  // complete
+                }
+              });
+
+
               typeof cb == "function" && cb(that.globalData.userInfo)
+            },
+            fail: function (res) {
+              console.error('get user info error!');
+              console.log(res);
             }
           })
+        },
+        fail: function (e) {
+          console.error('wx login error!');
+          console.log(e);
         }
       })
     }
   },
+
   globalData: {
     userInfo: null,
     busInfo: {
